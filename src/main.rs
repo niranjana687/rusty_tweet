91d0df70b8c65df8e8ed430811515213975e5372
niranjana687 <niranjana687@gmail.com>
2020-07-11 17:24:12 +0530

    access tokens

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,26 @@
-extern crate egg_mode;
+/*extern crate egg_mode;
+extern crate tokio;
 use egg_mode::user::show;
 
 
-async fn user_function() {
-    let token = Token(1274672431286775808-axPGIZ8FnMROdhr0H5sRl1TGwelO72);
-    let rustlang = show("rustlang", &token).await.unwrap();
-    println!("{} (@{})", rustlang.name, rustlang.screen_name);
-
-
-}
 
 fn main() {
+    let con_token = egg_mode::KeyPair::new("consumer key", "consumer secret");
+    let request_token = egg_mode::request_token(&con_token, "oob").unwrap();
+    let auth_url = egg_mode::authorize_url(&request_token);
 
-}
+}*/
+
+use egg_mode::auth;
+use egg_mode::user;
+use tokio::main;
+
+#[main]
+
+let con_token = egg_mode::KeyPair::new("4E9IhvYn8IVpAOQOopx9YUj8B", "VUdZ9EU2K75RU9rwXdPzsGBez1cq7dY3oIlCIfXOBs5MQaGx81");
+let access_token = egg_mode::KeyPair::new("1274672431286775808-axPGIZ8FnMROdhr0H5sRl1TGwelO72", "OSvvc6MdHu34JjOtxSjEVUGJh2NhVdEhpgqoKZqvpFFbn");
+let token = egg_mode::Token::Access{
+    consumer: con_token,
+    access: access_token,
+};
+
